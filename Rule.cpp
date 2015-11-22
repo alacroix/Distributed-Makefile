14cb01fadfb0a91e64b20f300d321b7ef854b4de
Adrien Lacroix <oni.dev.box@gmail.com>
2015-11-22 18:44:59 +0100

    Edit Rule class

--- Rule.cpp
+++ Rule.cpp
@@ -4,6 +4,7 @@ Rule::Rule(std::string name, std::vector<std::string> cmd, std::vector<std::stri
     this->name = name;
     this->cmd = cmd;
     this->dependencies = dependencies;
+    executed = false;
 }
 
 void Rule::printRule() {
@@ -21,6 +22,23 @@ void Rule::printRule() {
     }
 }
 
+void Rule::execute(std::map<std::string, Rule*> dictionary) {
+    for(std::vector<int>::size_type i = 0; i != cmd.size(); i++) {
+        std::string exec = cmd.at(i).substr(0, cmd.at(i).find(' '));
+
+        // If it's a local rule, add ./
+        if (dictionary.find(exec) != dictionary.end()) {
+            std::stringstream ss;
+            ss << "./" << cmd.at(i).c_str();
+            system(ss.str().c_str());
+        } else {
+            system(cmd.at(i).c_str());
+        }
+    }
+
+    executed = true;
+}
+
 std::string Rule::getName() {
     return this->name;
 }
@@ -31,4 +49,12 @@ std::vector<std::string> Rule::getCmd() {
 
 std::vector<std::string> Rule::getDependencies() {
     return this->dependencies;
+}
+
+bool Rule::hasDependencies() {
+    return (this->dependencies.size() != 0);
+}
+
+bool Rule::hasBeenExecuted() {
+    return executed;
 }
--- Rule.h
+++ Rule.h
@@ -3,20 +3,27 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
+#include <sstream>
 
 class Rule {
 public:
     Rule(std::string name, std::vector<std::string> cmd, std::vector<std::string> dependencies);
 
     void printRule();
+    void execute(std::map<std::string, Rule*> dictionary);
 
     std::string getName();
     std::vector<std::string> getCmd();
     std::vector<std::string> getDependencies();
+
+    bool hasDependencies();
+    bool hasBeenExecuted();
 private:
     std::string name;
     std::vector<std::string> cmd;
     std::vector<std::string> dependencies;
+    bool executed;
 };
 
 #endif //DISTRIBUTED_MAKEFILE_RULE_H
