43bf65aaabb178401bda90fd074e82f31bde9230
Adrien Lacroix <oni.dev.box@gmail.com>
2015-12-02 19:33:14 +0100

    Fix barrier problem + improve printing + add clean rule for premier-small

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -10,11 +10,17 @@ void Manager::execute() {
     execute_rule(initial);
 }
 
+std::string printCurrentThread() {
+    std::stringstream ss;
+    ss << "Thread " << omp_get_thread_num() + 1 << "/" << omp_get_num_threads() << " | ";
+    return ss.str();
+}
+
 void Manager::execute_rule(Rule *rule) {
     // If file already exists, do nothing
     if (rule->alreadyExists()) {
 #pragma omp critical
-        std::cout << rule->get_name() << " already exists." << std::endl;
+        std::cout << printCurrentThread() << rule->get_name() << " already exists." << std::endl;
         return;
     }
 
@@ -23,13 +29,10 @@ void Manager::execute_rule(Rule *rule) {
         std::vector<std::string> dependencies = rule->get_dependencies();
 #pragma omp parallel shared(dependencies)
         {
-#pragma omp for nowait
+#pragma omp for
             for (std::vector<int>::size_type i = 0; i < dependencies.size(); i++) {
                 std::string ds = dependencies.at(i);
-#pragma omp critical
-                {
-                    std::cout << "thread " << omp_get_thread_num() << " execute " << ds << std::endl;
-                }
+
                 // If dependency is a simple file like a .c (ie not a rule)
                 if (dictionary.find(ds) == dictionary.end()) {
                     // If file not found, throw exception
@@ -39,14 +42,19 @@ void Manager::execute_rule(Rule *rule) {
                 } else {
                     Rule *d = dictionary[ds];
                     if (!d->has_been_executed()) {
+#pragma omp critical
+                        {
+                            std::cout << printCurrentThread() << "executing " << ds << std::endl;
+                        }
                         execute_rule(d);
                     }
                 }
             }
+#pragma omp barrier
         }
     }
-#pragma omp barrier
+
     // Finally, execute itself
     rule->execute(dictionary);
-    std::cout << rule->get_name() << " done." << std::endl;
+    std::cout << printCurrentThread() << rule->get_name() << " done." << std::endl;
 }
